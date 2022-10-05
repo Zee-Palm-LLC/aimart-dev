ebd8c3f504f61f2aae4b593645967ba89abace03
tanzeel-zeepalm <tanzeelurrehman@zeepalm.com>
2022-10-05 16:02:48 +0500

    new landing

--- lib/app/data/constants/color.dart
+++ lib/app/data/constants/color.dart
@@ -1,15 +1,12 @@
 import 'package:flutter/cupertino.dart';
 
 class CustomColors {
-<<<<<<< HEAD
-  static Color kPrimary = const  Color(0xff4078E0);
-  static Color kBlack = const  Color(0xff192252);
-  static Color kBackground =const  Color(0xffFAFBFE);
   
-=======
   static Color kPrimary = Color(0xff4078E0);
   static Color kBlack = Color(0xff192252);
   static Color kBackground = Color(0xffFAFBFE);
   static Color kGrey = Color(0xff848FAC);
->>>>>>> 7710df867d036f75e73d28de228b5211209cb547
+   static Color kfullWhite = Color(0xffFFFFFF);
+
+
 }
